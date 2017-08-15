1ea5e63c5db5dc7d280b4427eaae4a9ade4fb642
HoEndo <huyindu@gmail.com>
2017-08-15 12:00:00 +0800

    ASP.NET Web API 搭建微信小程序后端

--- posts.js
+++ posts.js
@@ -1,2 +1,15 @@
 var POSTS_DATA = 
-[];
+[
+  {
+    "title": "ASP.NET Web API 搭建微信小程序后端",
+    "slug": "2017-08-15-aspnet-webapi-wechat-miniapp",
+    "date": "2017-08-15",
+    "tags": [
+      "ASP.NET",
+      "Web API",
+      "微信小程序",
+      "后端开发"
+    ],
+    "summary": "基于 ASP.NET Web API 2 实现微信小程序登录、支付和消息推送的后端对接"
+  }
+];
