96aa4a19dbd72af6e2bfa818378cd003e9892414
HoEndo <huyindu@gmail.com>
2017-10-22 12:00:00 +0800

    微信小程序开发踩坑记：从授权到地图组件

--- posts.js
+++ posts.js
@@ -1,6 +1,18 @@
 var POSTS_DATA = 
 [
   {
+    "title": "微信小程序开发踩坑记：从授权到地图组件",
+    "slug": "2017-10-22-wechat-miniapp-auth-map-pitfalls",
+    "date": "2017-10-22",
+    "tags": [
+      "微信小程序",
+      "前端开发",
+      "授权",
+      "地图"
+    ],
+    "summary": "小程序前端实战：授权流程、地图 API 集成中遇到的典型问题和解决方案"
+  },
+  {
     "title": "ASP.NET Web API 搭建微信小程序后端",
     "slug": "2017-08-15-aspnet-webapi-wechat-miniapp",
     "date": "2017-08-15",
